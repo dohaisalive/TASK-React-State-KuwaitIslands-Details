e151e5e7a51f64567375378783522e83162cddec
dohaisalive <dohaisalive@gmail.com>
2022-05-13 21:50:06 +0300

    done with task 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-
-import './App.css';
-import Header from './Components/Header';
-import IslandForm from './Components/IslandForm';
-import IslandList from './Components/IslandList';
-
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import IslandForm from "./Components/IslandForm";
+import IslandList from "./Components/IslandList";
+import islands from "./Components/islands";
 
 function App() {
+  const [islandState, setislandstate] = useState(islands);
 
-  let island = {
+  const [island, setIsland] = useState({
     name: "Boubyan",
-    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg"
-  }
+    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg",
+  });
+
+  const incVisitors = (id) => {
+    let newisland = islands.find((island) => island.id === id);
+    newisland.visitors = island.visitors + 1;
+    setIsland(newisland);
+  };
 
   return (
-
     <div>
       <Header />
 
       <div className="homePage">
-        <IslandList />
-        <IslandForm island={island} />
+        <IslandList setIsland={setIsland} islands={islandState} />
+        <IslandForm island={island} incVisitors={incVisitors} />
       </div>
     </div>
   );
--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -1,12 +1,36 @@
-export default function IslandForm({ island }) {
+import React, { useState } from "react";
+
+export default function IslandForm({ island, incVisitors }) {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState();
+
   return (
     <div className="form">
       <h2>{island.name}</h2>
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
-      <input placeholder="Type Full Name" />
-      <input placeholder="Type Phone Number" />
-      <button className="book" onClick={() => {}}>
+      <input
+        placeholder="Type Full Name"
+        onChange={(event) => setName(event.target.value)}
+      />
+      <input
+        type="tel"
+        pattern="[0-9]{8}"
+        placeholder="Type Phone Number"
+        onChange={(event) => setPhone(event.target.value)}
+      />
+      <button
+        className="book"
+        onClick={() => {
+          if (
+            window.confirm(
+              `Are you sure you want to book to ${island.name} with the name: ${name}, phone: ${phone}?`
+            )
+          ) {
+            incVisitors(island.id);
+          }
+        }}
+      >
         Book for today!
       </button>
     </div>
